2b99e1c8052457f4ee068d0b2e9f786926fd1c75
=Argenis Gutierrez <=argenis061274@hotmail.com>
2019-01-09 21:04:07 -0400

    pruebas de formularios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formbuilder';
+  form: FormGroup;
+  constructor(public formBuilder: FormBuilder ){
+    this.form = this.formBuilder.group({
+      nombre: ['',[Validators.required,Validators.maxLength(10)]],
+      apellido: ['',Validators.required],
+      email: ['',Validators.required]
+    });
+  }
+onFormSubmit(){
+  if(this.form.value.nombre == 'argenis'){
+    console.log("eres el mejor");
+  }else{
+    console.log("eres el peor");
+  }
+  console.log(this.form.value);
+}
 }
